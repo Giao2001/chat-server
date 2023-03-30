4e8513f71ff7efd01e2fc9221508878dc651fbbd
oilliever <baduy.work@gmail.com>
2023-03-30 17:21:07 +0700

    Set up friends feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,14 +14,37 @@ exports.updateMe = async (req, res, next) => {
     });
 };
 
-exports.getUser = async (reg, res, next) => {
+exports.getUsers = async (req, res, next) => {
     const all_users = await User.find({ verified: true }).select("firstName lastName _id");
 
-    const this_user = reg.user;
+    const this_user = req.user;
 
     const remaining_users = all_users.filter(
-        (user) => !this_user.friends.includes(user_id) && user._id.toString() !== req.user._id.toString()
+        (user) => !this_user.friends.includes(user._id) && user._id.toString() !== req.user._id.toString()
     );
 
-    res.status(200).json({ status: "success", data: remaining_users, message: "Users found successfully!" });
+    res.status(200).json({
+        status: "success",
+        data: remaining_users,
+        message: "Users found successfully!",
+    });
+};
+
+exports.getFriends = async (req, res, next) => {
+    const this_user = await User.findById(req.user._id).populate("friends", "_id firstName lastName");
+    res.status(200).json({
+        status: "success",
+        data: this_user.friends,
+        message: "Friends found successfully!",
+    });
+};
+
+exports.getRequests = async (req, res, next) => {
+    const requests = await FriendRequest.find({ recipient: req.user._id }).populate("sender").select("_id firstName lastName");
+
+    res.status(200).json({
+        status: "success",
+        data: requests,
+        message: "Requests found successfully!",
+    });
 };
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const authController = require("../controllers/authController");
 router.patch("/update-me", authController.protect, userController.updateMe);
 // router.post("/generate-zego-token", authController.protect, userController.generateZegoToken);
 router.get("/get-users", authController.protect, userController.getUsers);
-// router.get("/get-requests", authController.protect, userController.getRequests);
-// router.get("/get-friends", authController.protect, userController.getFriends);
+router.get("/get-requests", authController.protect, userController.getRequests);
+router.get("/get-friends", authController.protect, userController.getFriends);
 
 module.exports = router;
