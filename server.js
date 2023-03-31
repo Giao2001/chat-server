b5b6e1234577fd0491812bff414980da19386bce
oilliever <baduy.work@gmail.com>
2023-03-31 23:51:04 +0700

    build chat feature

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 const { Server } = require("socket.io");
+const FriendRequest = require("./models/friendRequest");
+const Message = require("./models/Message");
 
 dotenv.config({ path: "./config.env" });
 
@@ -12,6 +14,8 @@ process.on("uncaughtException", (err) => {
 });
 
 const http = require("http");
+const User = require("./models/user");
+// const { default: Message } = require("../ChatRealTime/src/components/Conversation/Message");
 
 const server = http.createServer(app);
 
@@ -50,19 +54,28 @@ io.on("connection", async (socket) => {
     const user_id = socket.handshake.query["user_id"];
     const socket_id = socket.id;
     console.log(`User connected ${socket_id}`);
-    if (user_id) {
-        await User.findByIdAndUpdate(user_id, { socket_id });
+    if (Boolean(user_id)) {
+        await User.findByIdAndUpdate(user_id, { socket_id, status: "Online" });
     }
 
     //socket event listener
 
     socket.on("friend_request", async (data) => {
-        console.log(data.to);
-        const to = await User.findById(date, to);
+        const to = await User.findById(data.to).select("socket_id");
+        const from = await User.findById(data.from).select("socket_id");
 
-        //create a friend request
-
-        io.to(to.socket_id).emit("new_friend_request", {});
+        // create a friend request
+        await FriendRequest.create({
+            sender: data.from,
+            recipient: data.to,
+        });
+        // emit event request received to recipient
+        io.to(to.socket_id).emit("new_friend_request", {
+            message: "New friend request received",
+        });
+        io.to(from.socket_id).emit("request_sent", {
+            message: "Request Sent successfully!",
+        });
     });
 
     socket.on("accept_request", async (data) => {
@@ -95,7 +108,104 @@ io.on("connection", async (socket) => {
         });
     });
 
-    socket.on("end", function () {
+    socket.on("get_direct_conversations", async ({ user_id }, callback) => {
+        const existing_conversations = await Message.find({
+            participants: { $all: [user_id] },
+        }).populate("participants", "firstName lastName _id email status");
+
+        console.log(existing_conversations);
+
+        callback(existing_conversations);
+    });
+
+    socket.on("start_conversation", async (data) => {
+        // data: {to: from:}
+
+        const { to, from } = data;
+
+        // check if there is any existing conversation
+
+        const existing_conversations = await Message.find({
+            participants: { $size: 2, $all: [to, from] },
+        }).populate("participants", "firstName lastName _id email status");
+
+        console.log(existing_conversations[0], "Existing Conversation");
+
+        // if no => create a new Message doc & emit event "start_chat" & send conversation details as payload
+        if (existing_conversations.length === 0) {
+            let new_chat = await Message.create({
+                participants: [to, from],
+            });
+
+            new_chat = await Message.findById(new_chat).populate("participants", "firstName lastName _id email status");
+
+            console.log(new_chat);
+
+            socket.emit("start_chat", new_chat);
+        }
+        // if yes => just emit event "start_chat" & send conversation details as payload
+        else {
+            socket.emit("start_chat", existing_conversations[0]);
+        }
+    });
+
+    socket.on("get_messages", async (data, callback) => {
+        const { messages } = await Message.findById(data.conversation_id).select("messages");
+        callback(messages);
+    });
+
+    // handle text/link message
+    socket.on("text_message", async (data) => {
+        console.log("Received message", data);
+
+        // data to, from, text, conversation_id, type
+        const { to, from, message, conversation_id, type } = data;
+        const new_message = { to, from, type, text: message, created_at: Date.now() };
+        const to_user = await User.findById(to);
+        const from_user = await User.findById(from);
+
+        // create new conversation if it doesnt exist yet or add new message to the message list
+        const chat = await Message.findById(conversation_id);
+        chat.messages.push(new_message);
+
+        // save to db
+        await chat.save({});
+
+        // emit incoming_message -> to used
+        io.to(to_user.socket_id).emit("new_message", { conversation_id, message: new_message });
+
+        // emit outgoing_message -> from user
+        io.to(from_user.socket_id).emit("new_message", { conversation_id, message: new_message });
+    });
+
+    socket.on("file_message", (data) => {
+        console.log("Received message", data);
+
+        // data: text, from, file
+
+        // get the file extension
+        const fileExtension = path.extname(data.file.name);
+
+        // generate a unique filname
+        const fileName = `${Date.now()}_${Math.floor(Math.random() * 10000)}${fileExtension}`;
+
+        // upload file to aws
+
+        // create new conversation if it doesnt exist yet or add new message to the message list
+
+        // save to db
+
+        // emit incoming_message -> to used
+
+        // emit outgoing_message -> from user
+    });
+
+    socket.on("end", async (data) => {
+        // find user by id and set status to offline
+        if (data.user_id) {
+            await User.findByIdAndUpdate(data.user_id, { status: "Offline" });
+        }
+        // boardcast user disconnected
         console.log("closing connection");
         socket.disconnect(0);
     });
